5e90fdea20265ab247e4683aa5cb328318a7d001
Germangguerci <germangguerci@gmail.com>
2021-01-19 19:01:19 -0300

    Form ready
    Parent_category, image, remains.

--- client/src/components/category/CategoryForm.jsx
+++ client/src/components/category/CategoryForm.jsx
@@ -4,6 +4,13 @@ import * as yup from 'yup';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+
+
 
 const validationSchema = yup.object({
   name: yup
@@ -19,7 +26,9 @@ const CategoryForm = () => {
   const formik = useFormik({
     initialValues: {
       name: null,
-      description: null
+      description: null,
+      parent_id: null,
+      image: null
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -27,33 +36,59 @@ const CategoryForm = () => {
     },
   });
 
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  const classes = useStyles();
+
+
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id="name"
-          name="name"
-          label="Name"
-          value={formik.values.name}
-          onChange={formik.handleChange}
-          error={formik.touched.name && Boolean(formik.errors.name)}
-          helperText={formik.touched.name && formik.errors.name}
-        />
-        <TextareaAutosize
-          fullWidth
-          id="description"
-          name="description"
-          label="Description"
-          value={formik.values.description}
-          onChange={formik.handleChange}
-          error={formik.touched.description && Boolean(formik.errors.description)}
-          helperText={formik.touched.description && formik.errors.description}
-        />
-         
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
+    <div className={classes.paper}>
+      <form onSubmit={formik.handleSubmit} className={classes.form}>
+      <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Typography component="h1" variant="h5">
+                New Category
+            </Typography>
+            <TextField
+            fullWidth
+            id="name"
+            name="name"
+            label="Name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.touched.name && Boolean(formik.errors.name)}
+            helperText={formik.touched.name && formik.errors.name}
+            />
+            <TextField
+            fullWidth
+            multiline
+            id="description"
+            name="description"
+            label="Description"
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            error={formik.touched.description && Boolean(formik.errors.description)}
+            helperText={formik.touched.description && formik.errors.description}
+            />
+            <Button color="primary" variant="contained" fullWidth type="submit" className={classes.submit}>
+            Submit
+            </Button>
+        </Container>    
+        
       </form>
     </div>
   );
