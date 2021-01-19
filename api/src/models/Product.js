9060f5c720a8233dc4212af1fac7d2f2e712e27e
Death97-byte <68304510+Death97-byte@users.noreply.github.com>
2021-01-19 16:51:41 -0300

    Task S4 done

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
+const { STRING, FLOAT, INTEGER, BOOLEAN, DECIMAL, BLOB } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('product', {
+    description: {
+      type: STRING,
+      allowNull: false
+    },
+    discount: {
+      type: INTEGER,
+      defaultValue: 0
+    },
+    featured: {
+      type: BOOLEAN,
+      defaultValue: false
+    },
+    image: {
+      type: BLOB,
+    },
     name: {
-      type: DataTypes.STRING,
+      type: STRING,
       allowNull: false,
+    },
+    price: {
+      type: DECIMAL,
+      allowNull: false
+    },
+    rating: {
+      type: FLOAT,
+    },
+    stock: {
+      type: INTEGER,
+      allowNull: false
     },
   });
 };
