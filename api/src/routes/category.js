260b7de49129a3fec1a6a401706b3c0aaa800f0d
dager2115 <71511850+dager2115@users.noreply.github.com>
2021-01-20 13:24:02 -0500

    Update category.js

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -2,10 +2,11 @@ const server = require('express').Router();
 const { Category } = require('../db.js');
 
 server.post('/', (req, res) =>{
-    Category.create(req.body)
+    Category.create(req.body.form)
     .then(category => {
         res.status(200).send(category)
     })
+    .catch(
 })
 
 module.exports = server;
