17b664138dc828a3a376bdc0371ec6927b4eca22
dager2115 <dager2115@gmail.com>
2021-01-21 11:09:14 -0500

    Update product.js

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,4 +1,5 @@
 const server = require('express').Router();
+const { response } = require('express');
 const { Product, Category } = require('../db.js');
 
 server.get('/', (req, res, next) => {
@@ -18,18 +19,38 @@ server.post('/', (req, res) =>{
         res.status(400).send(error)
     })
 })
+// this is from task 25 and it is incomplete, but i needed a small part of the path to do my task
 
-server.post('/:productId/category/:categoryId', (req, res) =>{
-	Product.findOne({
-		where: {id: req.params.productId}
+server.post('/:productId/category/:categoryId', async (req, res) =>{
+	const category =  await Category.findByPk(req.params.categoryId)
+	const product = await Product.findByPk(req.params.productId)
+
+	if(!category){res.status(404).send("this category doesn't exist")}
+	if(!product){res.status(404).send("this product doesn't exist")}
+
+	await category.addProduct(product)
+	.then(response =>{
+		res.send(response)
 	})
-	.then(response => res.send(response))
-	.catch(err => res.send(err))
-	
+	.catch(error =>{
+		res.send(error)
+	})
 })
 
-server.delete('/:productId/category/:categoryId', (req, res) =>{
-	
+server.delete('/:productId/category/:categoryId', async (req, res) =>{
+	const category =  await Category.findByPk(req.params.categoryId)
+	const product = await Product.findByPk(req.params.productId)
+
+	if(!category){res.status(404).send("this category doesn't exist")}
+	if(!product){res.status(404).send("this product doesn't exist")}
+
+	await product.removeCategory(category)
+	.then(response =>{
+		res.status(response).send("has been successfully removed")
+	})
+	.catch(error =>{
+		res.send(error)
+	})
 })
 
 module.exports = server;
