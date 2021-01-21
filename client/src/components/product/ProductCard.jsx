3d28185bf4c0e7096d71abac99af11519234dc25
laureanoSilva <laureano.bara@gmail.com>
2021-01-21 20:32:52 -0300

    product card mejorada. Agrego React Router

--- client/src/components/product/ProductCard.jsx
+++ client/src/components/product/ProductCard.jsx
@@ -1,34 +1,55 @@
 import React from 'react';
-import { Button, Container, CssBaseline, Typography, } from '@material-ui/core';
+import { Card, CardContent, CardActionArea, CardActions, CardMedia, CssBaseline, IconButton, Typography, } from '@material-ui/core';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import InfoIcon from '@material-ui/icons/Info';
 import { useStylesProductCard } from './styles';
+import { Link } from 'react-router-dom'
 
 function ProductCard({ productId, discount, featured, image, 
-                   name, price, stock, }) {
+                   name, price, stock, description}) {
              
     const style = useStylesProductCard();
                  
     return (
-        <Container component="main" maxWidth="xs">
+        
+        <Card className={style.root} component="main" maxWidth="xs">
+            <Link to={`/products/${productId}`} >
             <CssBaseline />
-            <div className={style.imgWrapper} onClick={() => alert("te llevo al detalle del producto?")}> {/*dispatchear la action para ir al detalle*/}
-                <div className={style.img}>
-                    <img src={ image } alt={ name }></img>
-                </div>
-                <div className={style.buttonContainer}>
-                    <Button color="primary" variant="contained" fullWidth type="submit" className={style.button}>ADD TO CART</Button>
-                    {/*<Button color="secondary" variant="contained" fullWidth type="submit" className={style.button}>VIEW DETAILS</Button>*/}
-                </div>
-                <div>
-                <Typography component="h3">{ name }</Typography>
+            <CardActionArea>
+        <CardMedia
+          className={style.media}
+          image={image}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2" className={style.rigthText}>
+            {name}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h2" className={style.rigthText} >
                 { discount !== 0 ? // ToDo sacar esto afuera. Y usarlo en todos los lugares donde lo necesitemos.
-                             <span> <span className={style.lineThrough}>${ price }</span>
-                                ${ price - ((discount / 100) * price) }
+                             <span > <span className={style.lineThrough}>$ { price } </span>
+                                <span >$ {  price - ((discount / 100) * price) }</span>
                              </span> 
-                             : <span>{ price }</span>
+                             : <span >${ price }</span>
                 }
-                </div>
-            </div>
-        </Container>
+          </Typography>
+          <Typography   variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      </Link> 
+      <CardActions>
+        <IconButton size="small" className={style.button} color="primary" onClick={() => { alert('clicked') }}>
+          <ShoppingCartIcon />
+        </IconButton>
+        <IconButton size="small" color="primary">
+        <Link to={`/products/${productId}`} >
+          <InfoIcon />
+          </Link>
+        </IconButton>
+      </CardActions>
+        </Card>
     );
 };
 
--- client/src/components/product/styles/index.js
+++ client/src/components/product/styles/index.js
@@ -2,35 +2,18 @@ import { makeStyles } from '@material-ui/core';
 
 
 export const useStylesProductCard = makeStyles(theme => ({
-    imgWrapper: {
-        position: 'relative',
-      },
-      buttonContainer: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignContent: 'end',
-        position: 'absolute',
-        opacity:0,
-        right: '30%',
-        bottom: 60,
-        left: '30%',
-        top: 0,
-        textAlign: 'center',
-        '&:before': {
-          content: ' ',
-          display: 'block',
-          height: "45%",
-          },
-        '&:hover': {
-          opacity:100,
-          },
-      },
-      button: {
-        margin: 10,
-        flex: '100%',
-      },
+
       lineThrough: {
         textDecoration: 'line-through',
-        padding: 10,
+      },
+      root: {
+        maxWidth: 350,
+      },
+      media: {
+        height: 150,
+      },
+      rigthText: {
+        textAlign: 'center',
+        textDecoration: 'none',
       }
       }))
