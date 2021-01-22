a3eea21c18a21fb9886062a3e305aaf891e24572
Germangguerci <germangguerci@gmail.com>
2021-01-21 23:13:56 -0300

    Update route for catalog.

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -5,12 +5,14 @@ import Product from '../components/product/Product'
 import ProductCard from '../components/product/ProductCard'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import image from '../resources/default-image.png'
+import Catalog from '../components/catalog/Catalog'
 
 function Routes(){
     return(
         <React.Fragment>
             <Route path="/products/:id" render = {() => <Product description={"descrpcion"} name={"product 2"} price= {20} stock= {30}/>} />
             <Route path="/productcard" render = {() => <ProductCard productId={1} description={"descripcion completa del producto. podrian ser varias lineas y deberia verse bien. espero que así sea."} name={"product 2"} price= {20} stock= {30} image = {image} discount={15}/>} />
+            <Route path="/catalog" component={Catalog} />
         </React.Fragment>
     )
 }
