9daecf9478c824de9272a0aee1112f4081815437
Germangguerci <germangguerci@gmail.com>
2021-01-23 00:01:35 -0300

    Catalog display product-cards Responsive

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import FilterCatalog from './FilterCatalog';
 import {Container, Grid} from '@material-ui/core';
+import ProductCard from '../product/ProductCard'
 
 const Catalog = (props) => {
 
@@ -18,8 +19,43 @@ const Catalog = (props) => {
 
     return (
         <Container>
-            <Grid>
-                <FilterCatalog updateFilter={updateFilter}/>
+            <Grid container spacing={3} justify="center">
+                <Grid item xs={12} sm={3}>
+                    <FilterCatalog updateFilter={updateFilter}/>
+                </Grid>
+                <Grid container item xs={12} sm={9} spacing={3} justify="center" alignItems="center">
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3} >
+                        <ProductCard productId={1} name="Vamp guano" discount={0} image="https://tabugrowshop.com.ar/wp-content/uploads/2018/06/VAMP-Guano-de-murcielago-Flora-1-10047.jpg" price={680}/>
+                    </Grid>
+                    
+                </Grid>
             </Grid>
         </Container>
     )
