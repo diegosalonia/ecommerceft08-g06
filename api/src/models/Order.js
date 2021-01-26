cb5e7ade6240ced243b5c153592a6857eddfb7f2
Death97-byte <68304510+Death97-byte@users.noreply.github.com>
2021-01-26 14:22:56 -0300

    get Order rutes done. I also made create route

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -2,10 +2,10 @@ const { INTEGER, ENUM } = require("sequelize")
 
 module.exports = (sequelize) => {
     sequelize.define('order', {
-        user_id: {
-            type: INTEGER,
-            allowNull: false
-        },
+        // user_id: {
+        //     type: INTEGER,
+        //     allowNull: false
+        // },
         status: {
             type: ENUM('approved', 'cancelled', 'pending', 'cart', 'created'),
             allowNull: false
--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -1,11 +1,27 @@
 const server = require('express').Router();
 const { Sequelize } = require('sequelize');
-// const Order = require('../models/')
+const { Order, User } = require('../db.js');
 
-server.get('/orders', (req, res) => {
+server.get('/search', (req, res) => {
+    const status = req.query.status;
+    
+    Order.findAll({
+        where: {
+            status: status
+        }
+    })
+    .then(orders => res.send(orders))
+    .catch(err => console.log(err));
+});
 
-})
+server.post('/', async (req, res) => {
+    const order = {status: req.body.order.status};
+    const user = await User.findByPk(req.body.order.userId);
+    const newOrder = await Order.create(order)
 
-
+    user.addOrder(newOrder)
+    .then(order => res.send(order))
+    .catch(err => console.log(err));
+});
 
 module.exports = server;
