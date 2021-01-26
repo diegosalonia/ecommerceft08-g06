092dd5ab120999ebb2ae7e16246d74dcca3528b9
Death97-byte <68304510+Death97-byte@users.noreply.github.com>
2021-01-26 11:36:11 -0300

    Building order routes

---
+++ api/src/routes/order.js
@@ -0,0 +1,11 @@
+const server = require('express').Router();
+const { Sequelize } = require('sequelize');
+// const Order = require('../models/')
+
+server.get('/orders', (req, res) => {
+
+})
+
+
+
+module.exports = server;
