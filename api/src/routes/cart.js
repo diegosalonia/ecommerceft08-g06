12394c74fc7e8a4077b572880d724bd899c92592
Death97-byte <68304510+Death97-byte@users.noreply.github.com>
2021-01-27 11:07:42 -0300

    Create route done

--- api/src/routes/cart.js
+++ api/src/routes/cart.js
@@ -2,33 +2,30 @@ const server = require('express').Router();
 const { User, Order, Product} = require('../db.js');
 
 server.post('/:idUser/cart', async (req, res)=>{
-    const product = await Product.findByPk(req.body.product.id)
-    const quantity = req.body.product.quantity
-    const price = parseInt((product.price * req.body.product.quantity), 10);
-    const user = await User.findByPk(req.params.idUser)
-    const order = await Order.findOrCreate({where:{userId: user.id}})
+    const product = await Product.findByPk(req.body.product.id);
+    const quantity = req.body.product.quantity;
+    const price = product.price;
+    const user = await User.findByPk(req.params.idUser);
+    let order = await Order.findOne({ where:{ userId: user.id, status: 'cart'} });
 
-    if(!order){res.status(400).send("this order doens't exist")}
-    if(!user){res.status(400).send("this user doens't exist")}
+    if (!order) {
+        order = await Order.create()
+        user.addOrder(order);
+    };
 
-    await product.addOrder(order,{through: {orderId:order.id,quantity, price}})
+    if(!user){ res.status(400).send("this user doens't exist") };
+
+    await product.addOrder(order, { through: { orderId: order.id, quantity, price } })
     .then(response =>{
-        res.send("todo bien")
+        res.send(response);
     })
     .catch(err =>{
         console.log(err)
         res.send({
             mgs:"todo mal",
             error: err
-        })
-    })
-
-})
-
-
-server.delete('/:idUser/cart/:productId', async (req,res)=>{
-
-
-})
+        });
+    });
+});
 
 module.exports = server;
