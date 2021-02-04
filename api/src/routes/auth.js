1f77ccbc57e2ea6b8a5f75a33322b9f903a01bf0
laureanoSilva <laureano.bara@gmail.com>
2021-02-04 19:54:27 -0300

    Added /me route

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const server = express.Router();
 const { User } = require("../db.js");
-const { Sequelize } = require("sequelize");
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
 const jwt = require ('jsonwebtoken')
@@ -9,30 +8,12 @@ const jwt = require ('jsonwebtoken')
 server.post('/login', (req, res, next) => {
     passport.authenticate('local', {session:false}, (err, user) => {
         if(user) {
-            console.log('AQUI EL USUARIO: ', user)
             const token = jwt.sign({user}, "secret");
             return res.status(200).json({ user, token });
         }
     }) 
     (req, res, next)
 })
-
-// server.post("/login", passport.authenticate("local", { session: false }), (req, res, next) => {
-//    ((err, user, info) => {
-//     if (user) {
-//         req.login(user, { session: false }, (err) => {
-//           if (err) {
-//           return res.status(400).json({ error: "error al iniciar sesion" });
-//         }
-//         const token = jwt.sign({user}, "secret");
-//         return res.status(200).json({ user, token });
-//       });
-//     } else {
-//         console.log('aqui info: ', info)
-//       return res.status(402).json({ info });
-//     }
-//   })(req, res, next)
-// });
 
 server.post("/logout", (req, res) => {
   console.log(req.isAuthenticated());
@@ -44,5 +25,15 @@ server.post("/logout", (req, res) => {
     res.status(400).send("No estabas logeado :/");
   }
 });
+
+server.get("/me", passport.authenticate('jwt',{session: false}),(req,res,next)=>{
+    User.findByPk(req.user)
+    .then(user => {
+		res.json(user);
+	})
+	.catch(err=>{
+		res.send(err);
+	})
+})
 
 module.exports = server;
