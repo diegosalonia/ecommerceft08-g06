3a5f5dece91d827c21c20d2cec74688dd1af7735
dager2115 <dager2115@gmail.com>
2021-02-12 15:05:51 -0500

    little changes

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -43,10 +43,10 @@ module.exports = (sequelize) => {
             defaultValue: "user"
         },
         shipping_address:{
-            type:INTEGER
+            type:STRING
         },
-        billing_addres:{
-            type:INTEGER
+        billing_address:{
+            type:STRING
         },
         email_notification:{
             type:BOOLEAN
