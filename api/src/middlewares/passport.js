9bb97ceb3586522a95979b8ccc4be6b3131c711a
diego martin Salonia <diegosalonia@gmail.com>
2021-02-13 19:30:50 -0300

    Update api/src/middlewares/passport.js
    Co-authored-by: Laureano Silva <laureano.bara@gmail.com>

--- api/src/middlewares/passport.js
+++ api/src/middlewares/passport.js
@@ -153,7 +153,6 @@ passport.use(
         }
         else {
             const createdUser = await User.create(user)
-            console.log('USUARIO CREADO: ', user)
             done(null, createdUser)
             
         }
