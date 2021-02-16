6b4a9757309d3befc25e39136ce3e189de52aa07
Wilmer21 <hachikoestick87@gmail.com>
2021-02-15 20:51:44 -0500

    user-profile
    verificado con pequeños cambios

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const { User } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: false}).then(() => {
   
   server.listen(3000, () => {
     console.log('%s listening at 3000'); // eslint-disable-line no-console
